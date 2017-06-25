a5d1884240a155ee6e5c089e02a66548db90eea1
Eashan Ashrafulzaman <eashanashrafulzaman@Eashans-Mac-mini.local>
2017-06-25 21:41:01 +0600

    Just Created a class

---
+++ JSONTEST/Nothing.swift
@@ -0,0 +1,13 @@
+//
+//  Nothing.swift
+//  JSONTEST
+//
+//  Created by Eashan Ashrafulzaman on 6/25/17.
+//  Copyright © 2017 Eashan Ashrafulzaman. All rights reserved.
+//
+
+import UIKit
+
+class Nothing: NSObject {
+
+}
